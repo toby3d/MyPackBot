6766cd827ab813e00e6928e02d0b472ce476d58d
Maxim Lebedev <git@toby3d.ru>
2017-12-28 16:46:22 +0500

    :bug: Fixed fallback locale from 'en-us' to 'en'

--- switch_locale.go
+++ switch_locale.go
@@ -5,14 +5,14 @@ import (
 	"github.com/nicksnyder/go-i18n/i18n" // Internationalization and localization
 )
 
-const langDefault = "en-us"
+const langFallback = "en"
 
 func switchLocale(langCode string) (T i18n.TranslateFunc, err error) {
 	log.Ln("Check", langCode, "localization")
 	T, err = i18n.Tfunc(langCode)
 	if err != nil {
-		log.Ln("Unsupported language, change to 'en-us' by default")
-		T, err = i18n.Tfunc(langDefault)
+		log.Ln("Unsupported language, change to ", langFallback, " by default")
+		T, err = i18n.Tfunc(langFallback)
 	}
 	return
 }

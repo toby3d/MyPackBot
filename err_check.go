4dfb50b346d27df1427d83a36d60e582b0cbae00
Maxim Lebedev <git@toby3d.ru>
2018-01-25 17:25:54 +0500

    :loud_sound: Write panic logs to journals

--- err_check.go
+++ err_check.go
@@ -1,8 +1,11 @@
 package main
 
+import "fmt"
+
 // errCheck helps debug critical errors without warnings from 'gocyclo' linter
 func errCheck(err error) {
 	if err != nil {
+		fmt.Sprintln(err.Error())
 		waitForwards.Wait() // Wait what all users get announcement message
 		panic(err.Error())
 	}

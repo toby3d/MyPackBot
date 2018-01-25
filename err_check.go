7c6aa96eff26613e98f31ef7713c592084c62a86
Maxim Lebedev <git@toby3d.ru>
2018-01-25 17:25:13 +0500

    :sparkles: Wait sending announcements to all users before panic

--- err_check.go
+++ err_check.go
@@ -3,6 +3,7 @@ package main
 // errCheck helps debug critical errors without warnings from 'gocyclo' linter
 func errCheck(err error) {
 	if err != nil {
+		waitForwards.Wait() // Wait what all users get announcement message
 		panic(err.Error())
 	}
 }
--- update_channel_post.go
+++ update_channel_post.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"sync"
 	"time"
 
 	log "github.com/kirillDanshin/dlog"
 	tg "github.com/toby3d/telegram"
 )
+
+var waitForwards = new(sync.WaitGroup)
 
 func updateChannelPost(post *tg.Message) {
 	if post.Chat.ID != channelID {
@@ -17,11 +20,13 @@ func updateChannelPost(post *tg.Message) {
 	errCheck(err)
 
 	for i := range users {
+		waitForwards.Add(1)
 		if _, err = bot.ForwardMessage(
 			tg.NewForwardMessage(post.Chat.ID, int64(users[i]), post.ID),
 		); err != nil {
 			log.Ln(err.Error())
 		}
+		waitForwards.Done()
 
 		time.Sleep(time.Second / 10) // For avoid spamming
 	}
